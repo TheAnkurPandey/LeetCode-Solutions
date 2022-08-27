dea0a3b9b894076a2010b99eb3c2e42fe47182eb
Ankur Pandey <ankur.pandey1996@gmail.com>
2022-08-27 18:47:54 +0530

    Time: 148 ms (27.19%), Space: 14.1 MB (95.60%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        length = len(nums)
+        used = [False] * length
+        partial = []
+        solutions =[]
+        
+        self.generate_permute_unique(nums, used, partial, solutions)
+        
+        return solutions
+    
+    def generate_permute_unique(self, nums: List[int], 
+                         used: List[bool], partial: List[int], 
+                         solutions:List[List[int]]) -> None:
+        # O(n!) time & O(n) space
+        
+        # Base case
+        if len(partial) == len(nums):
+            solutions.append(partial[::])
+            return
+        
+        # Recursive case
+        length = len(nums)
+        for i in range(length):
+            # Already part of partial
+            if used[i]:
+                continue
+                
+            # Leads to duplicate solution
+            if (i > 0) and (nums[i] == nums[i - 1]) and (not used[i - 1]):
+                continue
+            
+            # Take move
+            used[i] = True
+            partial.append(nums[i])
+            
+            self.generate_permute_unique(nums, used, partial, solutions)
+            
+            # Undo move
+            partial.pop()
+            used[i] = False
+            
+        return
