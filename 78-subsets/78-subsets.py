3c8b600b5f7b02b21b11611f26b2cb8cfb1c3a9a
Ankur Pandey <ankur.pandey1996@gmail.com>
2022-08-27 13:27:58 +0530

    Time: 56 ms (43.31%), Space: 14.2 MB (35.80%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,9 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        depth = 0
-        partial = []
+        # Way - 1
+        # depth = 0
+        # partial = []
+        # solutions = []
+        # self.generate_subsets(nums, depth, partial, solutions)
+        # return solutions
+        
+        # Way - 2
+        choosen = []
+        remaining = nums[:]
         solutions = []
-        self.generate_subsets(nums, depth, partial, solutions)
+        self.generate_subsets_2(choosen, remaining, solutions)
         return solutions
     
     def generate_subsets(self, nums: List[int], 
@@ -25,3 +33,27 @@ class Solution:
         partial.append(nums[depth])  # Take move
         self.generate_subsets(nums, depth + 1, partial, solutions)
         partial.pop()  # Undo move
+        
+    def generate_subsets_2(self, 
+                         choosen: List[int], remaining: List[int], 
+                         solutions: List[List[int]]) -> None:
+        # O(2^n) time & O(n) space
+        
+        # Base case
+        if not remaining:
+            solutions.append(choosen[:])
+            return 
+        
+        # Recursive case
+        item = remaining.pop(0)  # Take move
+        
+        # Take move
+        self.generate_subsets_2(choosen, remaining, solutions)
+        # undo move
+        
+        choosen.append(item)  # Take move
+        self.generate_subsets_2(choosen, remaining, solutions)
+        choosen.pop()  # Undo move
+        
+        remaining.insert(0, item)  # undo move
+        
