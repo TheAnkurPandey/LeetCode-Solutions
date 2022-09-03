9c43e34b9bb84bff51506435ca4f423447ac1f85
Ankur Pandey <ankur.pandey1996@gmail.com>
2022-09-03 14:18:13 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (66.96%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,6 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
-        return climbStairsTopDownClient(n);
+        // return climbStairsTopDownClient(n);
+        return climbStairsBottomUp(n);
     }
     
     private int climbStairsTopDownClient(int n) {
@@ -24,6 +25,26 @@ class Solution {
         int stepY = climbStairsTopDown(n - 2, countDP);  // Go down by 2 steps
         
         countDP[n] = stepX + stepY;
+        
+        return countDP[n];
+    }
+    
+    private int climbStairsBottomUp(int n) {
+        // Bottom up / Tabulation
+        // O(n) time & O(n) space
+        
+        int[] countDP = new int[n + 1];
+        
+        // Base case
+        countDP[0] = 1;
+        countDP[1] = 1;
+        
+        for (int i = 2; i < n+1; i++) {
+            int stepX = countDP[i - 1];  // Go down by 1 step
+            int stepY = countDP[i - 2];  // Go down by 2 steps 
+            
+            countDP[i] = stepX + stepY;
+        }
         
         return countDP[n];
     }
