b76120f8f15b4ba168f98571fdde91725a84e5e1
Ankur Pandey <ankur.pandey1996@gmail.com>
2022-09-03 12:54:48 +0530

    Time: 62 ms (64.27%), Space: 44.4 MB (56.58%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,6 +1,7 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return lengthOfLISTopDownClient(nums);
+        // return lengthOfLISTopDownClient(nums);
+        return lengthOfLISBottomUp(nums);
     }
     
     private int lengthOfLISTopDownClient(int[] nums) {
@@ -37,10 +38,10 @@ class Solution {
         
         // Recrusive case
         // Not computed yet
-        int lengthOfLISTillEnd = 1;  // Initial value of length of Longest LIS 
+        int lengthOfLISTillEnd = 1;  // Every element forms LIS of length atleast 1 
         
         for (int i = end - 1; i >= 0; i--) {
-            if (nums[end] > nums[i]) {  // Can end form LIS with ith item
+            if (nums[i] < nums[end]) {  // Can end form LIS with ith item
                 
                 // length of LIS by using LIS ending at i
                 int lengthOfLISTillI = lengthOfLISTopDown(i, nums, lisDP);
@@ -56,5 +57,43 @@ class Solution {
         lisDP[end] = lengthOfLISTillEnd;
         
         return lisDP[end];
+    }
+    
+    private int lengthOfLISBottomUp(int nums[]){
+        // Tabulation / Bottom up
+        // O(n^2) time & O(n) space
+        
+        int n = nums.length;
+        int[] lisDP = new int[n];
+        
+        // Compute LIS ending at each index
+        // Because LIS may end at any index
+        for (int end = 0; end < n; end++) {
+            int lengthOfLISTillEnd = 1;  // Every element forms LIS of length atleast 1
+            
+            for (int i = end - 1; i > -1; i--) {
+                if (nums[i] < nums[end]) {  // Can end form LIS with ith item
+                    
+                    // length of LIS by using LIS ending at i
+                    int lengthOfLISTillI = lisDP[i];
+                    int lengthOfLISUsingI = lengthOfLISTillI + 1;
+                                        
+                    // Update the length of longest LIS found ending at end
+                    lengthOfLISTillEnd = Math.max(
+                        lengthOfLISTillEnd, lengthOfLISUsingI);
+                }
+            }
+            
+            // Update table
+            lisDP[end] = lengthOfLISTillEnd;
+        }
+        
+        // Processing DP table
+        int lenOfLIS = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (lisDP[i] > lenOfLIS) lenOfLIS = lisDP[i];
+        }
+        
+        return lenOfLIS;
     }
 }
