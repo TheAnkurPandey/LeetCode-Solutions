443760506ced8ccebb7b3baff772e535466a4def
Ankur Pandey <ankur.pandey1996@gmail.com>
2022-09-07 20:53:14 +0530

    Time: 2 ms (47.40%), Space: 54.5 MB (14.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,7 +1,8 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         // setZeroesSolA(matrix);
-        setZeroesSolB(matrix);
+        // setZeroesSolB(matrix);
+        setZeroesSolC(matrix);
     }
     
     private void setZeroesSolA(int[][] matrix) {
@@ -56,6 +57,7 @@ class Solution {
         boolean[] rowZerosMarker = new boolean[m];
         boolean[] colZerosMarker = new boolean[n];
         
+        // Getting value of zerosmarker for row & column
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (matrix[r][c] == 0) {
@@ -78,5 +80,44 @@ class Solution {
                 for (int r = 0; r < m; r++) matrix[r][c] = 0;
             }
         } 
+    }
+    
+    private void setZeroesSolC(int[][] matrix) {
+        // O(m + n) time & O(1) space
+        
+        int rMax = matrix.length;  // # Rows
+        int cMax = matrix[0].length;  // # Columns
+        
+        int firstRowMarker = 1;
+        
+        // Getting values of row & column markers
+        // Marker fields are already scanned before setting its value based on other values
+        for (int r = 0; r < rMax; r++) {
+            for (int c = 0; c < cMax; c++) {
+                if (matrix[r][c] == 0) {
+                    if (r == 0) firstRowMarker = 0;
+                    else matrix[r][0] = 0;  // Set the row marker to 0
+                    matrix[0][c] = 0;  // Set the col marker to 0
+                }
+            }
+        }
+        
+        // Setting value of Matrix based on marker
+        // Caution: make sure marker field are set to zeros at the end
+        for (int r = 1; r < rMax; r++) {
+            if (matrix[r][0] == 0) {
+                for (int c = 1; c < cMax; c++) matrix[r][c] = 0;
+            }
+        }
+        
+        for (int c = 0; c < cMax; c++) {
+            if (matrix[0][c] == 0) {  // Set entire column as zero
+                for (int r = 1; r < rMax; r++) matrix[r][c] = 0;
+            }
+        }        
+        
+        if (firstRowMarker == 0) {  // Set entire first row as zero
+            for (int c = 0; c < cMax; c++) matrix[0][c] = 0;
+        }
     }
 }
