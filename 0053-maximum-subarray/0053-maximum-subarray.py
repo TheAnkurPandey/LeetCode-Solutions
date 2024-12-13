8e56ecbe203d5077fe2910cba447b45ea06d0b9f
Ankur Pandey <ankur.pandey1996@gmail.com>
2024-12-13 18:52:05 +0530

    Time: 59 ms (60.32%), Space: 31.7 MB (12.45%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # tc: O(n); sc: O(1)
+        
+        maxSubArraySum = float('-inf')
+        maxSubArraySumEndingHere = 0
+        for i, v in enumerate(nums):
+            # nums[i] can be part of current subarray
+            if maxSubArraySumEndingHere > 0:
+                maxSubArraySumEndingHere = maxSubArraySumEndingHere + v
+            # nums[i] is not part of current subarray
+            else:
+                maxSubArraySumEndingHere = v
+                
+            maxSubArraySum = max(maxSubArraySum, maxSubArraySumEndingHere)
+            
+        return maxSubArraySum
+        
