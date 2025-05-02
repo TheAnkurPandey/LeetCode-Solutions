9891d8de37d1906c753ce5819fb8d9e8e8918f5f
Ankur Pandey <ankur.pandey1996@gmail.com>
2025-05-02 22:31:18 +0530

    Time: 44 ms (17.4%), Space: 18 MB (8.31%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,17 +1,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         # TC: O(max(m, n)); SC: O(1) 
-        res = ""
+        res = []
         l1, l2 = len(word1), len(word2)
         overlap_len = min(l1, l2)
+
         for i in range(overlap_len):
-            res += word1[i] + word2[i]
+            res.append(word1[i] + word2[i])
         
         if (l1 > l2):
-            res += word1[overlap_len :]
+            res.append(word1[overlap_len :])
         else:
-            res += word2[overlap_len :]
+            res.append(word2[overlap_len :])
 
-        return res
+        return "".join(res)
 
         
